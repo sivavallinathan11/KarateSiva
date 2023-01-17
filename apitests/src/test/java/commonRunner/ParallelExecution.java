33406db7ce943d59d08a7c9a257960c4214d4414
Sivavallinathan Palaniappan <spalaniappan@discoveryparks.com.au>
2023-01-17 16:48:29 +1030

    common runners setup

--- apitests/src/test/java/commonRunner/ParallelExecution.java
+++ apitests/src/test/java/commonRunner/ParallelExecution.java
@@ -1,17 +1,15 @@
-package examples;
+package commonRunner;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class ExamplesTest {
+class ParallelExecution {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:examples")
-                //.outputCucumberJson(true)
-                .parallel(5);
+        Results results = Runner.path("classpath:benefitsValidation").parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 

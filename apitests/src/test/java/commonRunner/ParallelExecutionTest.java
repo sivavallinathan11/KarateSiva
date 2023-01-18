37b66167f5c35c9c6c3ca890db664aa88cde55c8
Sivavallinathan Palaniappan <spalaniappan@discoveryparks.com.au>
2023-01-18 12:08:40 +1030

    parallelexecutionremoval

--- apitests/src/test/java/commonRunner/ParallelExecutionTest.java
+++ apitests/src/test/java/commonRunner/ParallelExecutionTest.java
@@ -1,16 +1,16 @@
-package commonRunner;
-
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
-class ParallelExecutionTest {
-
-    @Test
-    void testParallel() {
-        Results results = Runner.path("classpath:benefitsValidation").parallel(5);
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
-    }
-
-}
+//package commonRunner;
+//
+//import com.intuit.karate.Results;
+//import com.intuit.karate.Runner;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+//
+//class ParallelExecutionTest {
+//
+//    @Test
+//    void testParallel() {
+//        Results results = Runner.path("classpath:memberValidations").parallel(5);
+//        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+//    }
+//
+//}

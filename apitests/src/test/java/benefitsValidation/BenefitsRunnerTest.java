89482a912bf0f626a7fa7cdc1523a5c3dd6237eb
Sivavallinathan Palaniappan <spalaniappan@discoveryparks.com.au>
2023-02-02 12:51:35 +1030

    Karateconfigshanges

--- apitests/src/test/java/benefitsValidation/BenefitsRunnerTest.java
+++ apitests/src/test/java/benefitsValidation/BenefitsRunnerTest.java
@@ -5,7 +5,7 @@ import com.intuit.karate.junit5.Karate;
 public class BenefitsRunnerTest {
 
 	@Karate.Test
-    Karate testMembership() {
+    Karate testBenefits() {
         return Karate.run("benefits").tags("smoke").relativeTo(getClass());
     }
 }

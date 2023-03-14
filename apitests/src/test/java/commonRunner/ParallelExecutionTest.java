fc24d64fbc7574ff25913b110f4ad0bf5b951845
Gordon Massey <gmassey@discoveryparks.com.au>
2023-03-14 16:39:03 +1030

    parallel runner implemented
    Will run all listed features in parallel

--- apitests/src/test/java/commonRunner/ParallelExecutionTest.java
+++ apitests/src/test/java/commonRunner/ParallelExecutionTest.java
@@ -1,16 +1,28 @@
-//package commonRunner;
+package commonRunner;
 //
-//import com.intuit.karate.Results;
-//import com.intuit.karate.Runner;
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//
-//class ParallelExecutionTest {
-//
-//    @Test
-//    void testParallel() {
-//        Results results = Runner.path("classpath:memberValidations").parallel(5);
-//        assertEquals(0, results.getFailCount(), results.getErrorMessages());
-//    }
-//
-//}
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ParallelExecutionTest {
+
+	@Test
+    void testParallel() {
+        Results results = Runner.path(
+        		"classpath:couponValidations",
+        		"classpath:benefitsValidation",
+        		"classpath:deviceValidation",
+        		"classpath:fuelValidations",
+        		"classpath:leaderboardValidation",
+        		"classpath:loyaltyValidation",
+        		"classpath:memberBenefitsValidation",
+        		"classpath:memberRewardsValidation",
+        		"classpath:membershipValidation",
+        		"classpath:memberValidations",
+        		"classpath:personalDetailsValidation",
+        		"classpath:rewardsProgramValidation",
+        		"classpath:subscriptionValidation").tags("~@skipme").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+}
